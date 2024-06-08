6abb863d9d20ebf5df24875551a3ab1ca20ea2f4
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-06-08 20:29:14 +0530

    Editor Ouput added in Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,6 +2,7 @@ import { Post, User, Vote } from "@prisma/client";
 import { FC, useRef } from "react";
 import { formatTimeToNow } from "@/lib/utils";
 import { MessageSquare } from "lucide-react";
+import EditorOutput from "./EditorOutput";
 
 interface PostProps {
   subredditName: string;
@@ -44,6 +45,7 @@ const PostComp: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
             className="relative text-sm max-h-40 w-full overflow-clip"
             ref={pRef}
           >
+            <EditorOutput content={post.content} />
             {pRef.current?.clientHeight === 160 && (
               <div className="absolute  bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent" />
             )}
