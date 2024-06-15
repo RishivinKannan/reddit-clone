91e534499cb2b0e4cb8899bbe7ab8a1a9b46056f
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-06-15 19:38:39 +0530

    Search bar added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,24 +3,26 @@ import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const NavBar = async () => {
   const session = await getAuthSession();
   return (
     <nav className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+      <div className="container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
         {/* Logo */}
-        <Link href={"/"} className="flex gap-2 items-center">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-          <p className="hidden text-zinc-700  text-base font-semibold md:block">
+        <Link href={"/"} className="flex items-center gap-2">
+          <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-base font-semibold text-zinc-700 md:block">
             Breadit
           </p>
         </Link>
 
         {/* SearchBar */}
-
+        <SearchBar />
+        
         {session?.user ? (
-          <UserAccountNav user={session.user}/>
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="/sign-in" className={buttonVariants()}>
             Sign In
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { FC, useCallback, useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Prisma, Subreddit } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+import debounce from "lodash.debounce";
+
+interface SearchBarProps {}
+
+const SearchBar: FC<SearchBarProps> = ({}) => {
+  const [input, setInput] = useState("");
+
+  const request = debounce(async () => {
+    refetch();
+  }, 300);
+
+  const debounceRequest = useCallback(() => {
+    request();
+  }, []);
+
+  const router = useRouter();
+
+  const {
+    data: queryResults,
+    refetch,
+    isFetched,
+    isFetching,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return [];
+      const { data } = await axios.get(`/api/search?q=${input}`);
+      return data as (Subreddit & {
+        _count: Prisma.SubredditCountOutputType;
+      })[];
+    },
+    queryKey: ["search-query"],
+    enabled: false,
+  });
+  return (
+    <Command className="relative z-50 max-w-lg overflow-visible border">
+      <CommandInput
+        value={input}
+        onValueChange={(text) => {
+          setInput(text);
+          debounceRequest();
+        }}
+        className="border-none outline-none focus:border-none focus:outline-none ring-0"
+        placeholder="Search communities"
+      />
+      {input.length > 0 && (
+        <CommandList className="absolute inset-x-0 bg-white shadow top-full rounded-b-md">
+          {isFetched && <CommandEmpty>No results found</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 && (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  key={subreddit.id}
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                  value={subreddit.name}
+                >
+                  <Users className="w-4 h-4 mr-2" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
+        </CommandList>
+      )}
+    </Command>
+  );
+};
+
+export default SearchBar;
